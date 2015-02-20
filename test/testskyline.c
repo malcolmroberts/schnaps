ce343cb25627ff8823820dd7904bf7c95afaffe5
ph <helluy@unistra.fr>
2015-02-20 09:05:18 +0100

    clean skyline

--- test/testskyline.c
+++ test/testskyline.c
@@ -30,6 +30,8 @@ int TestSkyline(void){
 
 #define _NN 5
   
+  // preliminary work on the skyline struct
+  // _NN is the size of the linear system to be solved
   InitSkyline(&sky,_NN);
 
   double A[_NN][_NN];
@@ -68,7 +70,7 @@ int TestSkyline(void){
 
 
 
-
+  // first mark the nonzero values in A
   for(int i=0;i<_NN;i++){
     for(int j=0;j<_NN;j++){
       if (A[i][j] != 0) SwitchOn(&sky,i,j);
@@ -76,8 +78,10 @@ int TestSkyline(void){
     }
   }
 
+  // once the nonzero positions are known allocate memory
   AllocateSkyline(&sky);
 
+  // now set the nonzero terms
   for(int i=0;i<_NN;i++){
     for(int j=0;j<_NN;j++){
       if (A[i][j] != 0){
@@ -89,14 +93,19 @@ int TestSkyline(void){
     }
   }
 
-
+  // LU decomposition
   FactoLU(&sky);
 
+  // printf for checking...
   DisplaySkyline(&sky);
 
+  // solve from a decomposed matrix
+  // vf: rhs
+  // sol: solution
   SolveSkyline(&sky,vf,sol);
 
 
+  // checking
   double verr=0;
   printf("sol=");
   for(int i=0;i<_NN;i++){
@@ -105,9 +114,95 @@ int TestSkyline(void){
   }
   printf("\n");
 
+  // deallocate memory
   FreeSkyline(&sky);
 
   test= (verr<1e-10);
+  //assert(1==2);
+
+  // now test a symmetric matrix
+  A[0][0] = 0.3e1;
+  A[0][1] = -0.1e1;
+  A[0][2] = 0;
+  A[0][3] = 0;
+  A[0][4] = -0.1e1;
+  A[1][0] = -0.1e1;
+  A[1][1] = 0.2e1;
+  A[1][2] = -0.1e1;
+  A[1][3] = 0;
+  A[1][4] = 0;
+  A[2][0] = 0;
+  A[2][1] = -0.1e1;
+  A[2][2] = 0.2e1;
+  A[2][3] = -0.1e1;
+  A[2][4] = 0;
+  A[3][0] = 0;
+  A[3][1] = 0;
+  A[3][2] = -0.1e1;
+  A[3][3] = 0.2e1;
+  A[3][4] = -0.1e1;
+  A[4][0] = -0.1e1;
+  A[4][1] = 0;
+  A[4][2] = 0;
+  A[4][3] = -0.1e1;
+  A[4][4] = 0.3e1;
+  vf[0] = -0.4e1;
+  vf[1] = 0;
+  vf[2] = 0;
+  vf[3] = 0;
+  vf[4] = 0.10e2;
+
+  sky.is_sym=true;
+
+  // first mark the nonzero values in A
+  for(int i=0;i<_NN;i++){
+    for(int j=0;j<_NN;j++){
+      if ((A[i][j] != 0) && (j >= i)) SwitchOn(&sky,i,j);
+      //if (i==j) SwitchOn(&sky,i,j);
+    }
+  }
+
+  // once the nonzero positions are known allocate memory
+  AllocateSkyline(&sky);
+
+  // now set the nonzero terms
+  for(int i=0;i<_NN;i++){
+    for(int j=0;j<_NN;j++){
+      if ((A[i][j] != 0) && (j >= i)){
+      	SetSkyline(&sky,i,j,A[i][j]);
+      }
+      /* if (i==j){ */
+      /* 	SetSkyline(&sky,i,j,2); */
+      /* } */
+    }
+  }
+
+  // LU decomposition
+  FactoLU(&sky);
+
+  // printf for checking...
+  DisplaySkyline(&sky);
+
+  // solve from a decomposed matrix
+  // vf: rhs
+  // sol: solution
+  SolveSkyline(&sky,vf,sol);
+
+
+  // checking
+  verr=0;
+  printf("sol=");
+  for(int i=0;i<_NN;i++){
+    printf("%f ",sol[i]);
+    verr+=fabs(sol[i]-i-1);
+  }
+  printf("\n");
+
+  // deallocate memory
+  FreeSkyline(&sky);
+
+  test= test && (verr<1e-10);
+
 
   return test;
 
