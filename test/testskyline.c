c71950084a978738d0430a9ab8f4bbc19bcf2314
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2015-03-17 14:06:42 +0100

    solver skyline ok
    Conflicts:
    	test/testcollision.c

--- test/testskyline.c
+++ test/testskyline.c
@@ -33,37 +33,46 @@ int TestSkyline(void){
   InitSkyline(&sky,_NN);
 
   double A[_NN][_NN];
+  double vf[_NN],sol[_NN];
 
-  A[0][0] = 2;
-  A[0][1] = -0.5000000000e0;
+  A[0][0] = 0.2e1;
+  A[0][1] = -0.1e1;
   A[0][2] = 0;
   A[0][3] = 0;
-  A[0][4] = -0.5000000000e0;
-  A[1][0] = -1;
-  A[1][1] = 2;
-  A[1][2] = -1;
+  A[0][4] = 0;
+  A[1][0] = -0.1e1;
+  A[1][1] = 0.2e1;
+  A[1][2] = -0.1e1;
   A[1][3] = 0;
   A[1][4] = 0;
   A[2][0] = 0;
-  A[2][1] = -1;
-  A[2][2] = 2;
-  A[2][3] = -1;
+  A[2][1] = -0.1e1;
+  A[2][2] = 0.2e1;
+  A[2][3] = -0.1e1;
   A[2][4] = 0;
   A[3][0] = 0;
   A[3][1] = 0;
-  A[3][2] = -1;
-  A[3][3] = 2;
-  A[3][4] = -1;
+  A[3][2] = -0.1e1;
+  A[3][3] = 0.2e1;
+  A[3][4] = -0.1e1;
   A[4][0] = 0;
   A[4][1] = 0;
   A[4][2] = 0;
-  A[4][3] = -1;
-  A[4][4] = 2;
+  A[4][3] = -0.1e1;
+  A[4][4] = 0.2e1;
+  vf[0] = 0;
+  vf[1] = 0;
+  vf[2] = 0;
+  vf[3] = 0;
+  vf[4] = 0.6e1;
+
+
 
 
   for(int i=0;i<_NN;i++){
     for(int j=0;j<_NN;j++){
       if (A[i][j] != 0) SwitchOn(&sky,i,j);
+      //if (i==j) SwitchOn(&sky,i,j);
     }
   }
 
@@ -72,18 +81,33 @@ int TestSkyline(void){
   for(int i=0;i<_NN;i++){
     for(int j=0;j<_NN;j++){
       if (A[i][j] != 0){
-	SetSkyline(&sky,i,j,A[i][j]);
+      	SetSkyline(&sky,i,j,A[i][j]);
       }
+      /* if (i==j){ */
+      /* 	SetSkyline(&sky,i,j,2); */
+      /* } */
     }
   }
 
 
-  //FactoLU(&sky);
+  FactoLU(&sky);
 
   DisplaySkyline(&sky);
 
+  SolveSkyline(&sky,vf,sol);
+
+
+  double verr=0;
+  printf("sol=");
+  for(int i=0;i<_NN;i++){
+    printf("%f ",sol[i]);
+    verr+=fabs(sol[i]-i-1);
+  }
+  printf("\n");
+
   FreeSkyline(&sky);
 
+  test= (verr<1e-10);
 
   return test;
 
