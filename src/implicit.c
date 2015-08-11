7105dd1a66a8442242aabe29065f83717be7069a
efranck21 <franck@m-franck.u-strasbg.fr>
2015-08-11 13:45:10 +0200

    continue JK implicit for shallow water (not finish)

--- src/implicit.c
+++ src/implicit.c
@@ -215,9 +215,7 @@ void ThetaTimeScheme_WithJF(Simulation *simu, real tmax, real dt){
 
 }
 
-void NonlinearThetaVector_assembly(Simulation * simu,real * solvector,real *nlvector,real theta, real dt){
 
-  }
 
 
 
@@ -1116,5 +1114,368 @@ void InterfaceAssembly(Simulation *simu,  LinearSolver *solver,real theta, real 
     } // ipgfl
 
   } // macroface loop
+
+}
+
+
+
+
+
+void NonlinearThetaVector_assembly(Simulation * simu,real * solvector,real *nlvector,real theta, real dt){
+  const unsigned int m = simu->fd[0].model.m;
+    int fsize =  simu->wsize / simu->macromesh.nbelems;
+
+  
+  for(int ifa = 0; ifa < simu->macromesh.nbfaces; ifa++){
+    int ieL = simu->macromesh.face2elem[4 * ifa + 0];
+    int locfaL = simu->macromesh.face2elem[4 * ifa + 1];
+    int ieR = simu->macromesh.face2elem[4 * ifa + 2];
+    field *fL = simu->fd + ieL;
+    field *fR = NULL;
+    int offsetR = -1;
+    //printf("iel=%d ier=%d\n",ieL,ieR);
+    int offsetL = fsize * ieL;
+    if (ieR >= 0) {
+      fR = simu->fd + ieR;
+      offsetR = fsize * ieR;
+    }
+    
+    real *fwL = solvector + offsetL;
+    real *fwR = solvector + offsetR;
+    
+    real *nlvL = nlvector + offsetL;
+    real *nlvR = nlvector + offsetR;
+    
+    for(int ipgfL = 0; ipgfL < NPGF(fL->deg, fL->raf, locfaL); ipgfL++) {
+      
+      real xpgref[3], xpgref_in[3], wpg;
+
+      // Get the coordinates of the Gauss point and coordinates of a
+      // point slightly inside the opposite element in xref_in
+      int ipgL = ref_pg_face(fL->deg, fL->raf, locfaL, ipgfL, xpgref, &wpg, xpgref_in);
+      
+      real flux[m];
+      real wL[m];
+      
+      // Normal vector at gauss point ipgL
+      real vnds[3], xpg[3];
+      {
+	real dtau[3][3], codtau[3][3];
+	Ref2Phy(fL->physnode,
+		xpgref,
+		NULL, locfaL, // dpsiref, ifa
+		xpg, dtau,
+		codtau, NULL, vnds); // codtau, dpsi, vnds
+      }
+
+      if (fR != NULL) {  // the right element exists
+	real xrefL[3];
+	{
+	  real xpg_in[3];
+	  Ref2Phy(fL->physnode,
+		  xpgref_in,
+		  NULL, -1, // dpsiref, ifa
+		  xpg_in, NULL,
+		  NULL, NULL, NULL); // codtau, dpsi, vnds
+	  PeriodicCorrection(xpg_in,fL->period);
+	  Phy2Ref(fR->physnode, xpg_in, xrefL);
+
+	}
+	
+	int ipgR = ref_ipg(fR->deg,fR->raf, xrefL);
+	
+	real wR[m];
+	for(int iv = 0; iv < m; iv++) {
+	  int imemL = fL->varindex(fL->deg, fL->raf,fL->model.m, ipgL, iv);
+	  wL[iv] = fwL[imemL];
+	  int imemR = fR->varindex(fR->deg, fR->raf,fR->model.m, ipgR, iv);
+	  wR[iv] = fwR[imemR];
+	}
+
+	fL->model.NumFlux(wL, wR, vnds, flux);
+
+	// Add flux to both sides
+	for(int iv = 0; iv < m; iv++) {
+	  // The basis functions is also the gauss point index
+	  int imemL = fL->varindex(fL->deg, fL->raf,fL->model.m, ipgL, iv);
+	  int imemR = fR->varindex(fR->deg, fR->raf,fR->model.m, ipgR, iv);
+	  nlvL[imemL] -= flux[iv] * wpg;
+	  nlvR[imemR] += flux[iv] * wpg;
+	}
+	
+      }
+      else
+	{ // The point is on the boundary.
+	  for(int iv = 0; iv < m; iv++) {
+	    int imemL = fL->varindex(fL->deg, fL->raf,fL->model.m, ipgL, iv);
+	    wL[iv] = fwL[imemL];
+	  }
+
+
+	  fL->model.BoundaryFlux(xpg, fL->tnow, wL, vnds, flux);
+
+	  for(int iv = 0; iv < m; iv++) {
+	// The basis functions is also the gauss point index
+	    int imemL = fL->varindex(fL->deg, fL->raf,fL->model.m, ipgL, iv);
+	    nlvL[imemL] -= flux[iv] * wpg;
+	  }
+	}
+
+    }
+
+ }
+
+  for(int ie = 0; ie < simu->macromesh.nbelems; ++ie) {
+    field * f = simu->fd + ie;
+    real * w = solvector + ie * fsize;
+    real * nlv = nlvector + ie * fsize;
+
+    const int nraf[3] = {f->raf[0],
+		       f->raf[1],
+		       f->raf[2]};
+    const int deg[3] = {f->deg[0],
+		      f->deg[1],
+		      f->deg[2]};
+    const int npg[3] = {deg[0] + 1,
+		      deg[1] + 1,
+		      deg[2] + 1};
+
+    const unsigned int sc_npg = npg[0] * npg[1] * npg[2];
+
+
+    
+    for(int icL0 = 0; icL0 < nraf[0]; icL0++) {
+      for(int icL1 = 0; icL1 < nraf[1]; icL1++) {
+	for(int icL2 = 0; icL2 < nraf[2]; icL2++) {
+	  
+	  int icL[3] = {icL0, icL1, icL2};
+	  
+	  // Get the left subcell id
+	  int ncL = icL[0] + nraf[0] * (icL[1] + nraf[1] * icL[2]);
+	  // First glop index in the subcell
+	  int offsetL = npg[0] * npg[1] * npg[2] * ncL;
+
+          ///////////// Subcellinterface part //////////////////////
+	// Sweeping subcell faces in the three directions
+	  for(int dim0 = 0; dim0 < 3; dim0++) { 
+	    
+	    // Compute the subface flux only if we do not touch the
+	  // subcell boundary along the current direction dim0
+	    if (icL[dim0] != nraf[dim0] - 1) {
+	      int icR[3] = {icL[0], icL[1], icL[2]};
+	      // The right cell index corresponds to an increment in
+	      // the dim0 direction
+	      icR[dim0]++;
+	      int ncR = icR[0] + nraf[0] * (icR[1] + nraf[1] * icR[2]);
+	      int offsetR = npg[0] * npg[1] * npg[2] * ncR;
+	      
+	      // FIXME: write only write to L-values (and do both
+	      // faces) to parallelise better.
+	      
+	      const int altdim1[3] = {1, 0, 0};
+	      const int altdim2[3] = {2, 2, 1};
+
+	      // now loop on the left glops of the subface
+	    //int dim1 = (dim0 + 1)%3, dim2 = (dim0+2)%3;
+	      int dim1 = altdim1[dim0];
+	      int dim2 = altdim2[dim0];
+	      int iL[3];
+	      iL[dim0] = deg[dim0];
+	      for(iL[dim2] = 0; iL[dim2] < npg[dim2]; iL[dim2]++) {
+		for(iL[dim1] = 0; iL[dim1] < npg[dim1]; iL[dim1]++) {
+		// find the right and left glops volume indices
+		  
+		  int iR[3] = {iL[0], iL[1], iL[2]};
+		  iR[dim0] = 0;
+		  
+		  int ipgL = offsetL 
+		    + iL[0] + (deg[0] + 1) * (iL[1] + (deg[1] + 1) * iL[2]);
+		  int ipgR = offsetR 
+		    + iR[0] + (deg[0] + 1) * (iR[1] + (deg[1] + 1) * iR[2]);
+		  //printf("ipgL=%d ipgR=%d\n", ipgL, ipgR);
+		  
+		  // Compute the normal vector for integrating on the
+		  // face
+		  real vnds[3];
+		  {
+		    real xref[3], wpg3;
+		    ref_pg_vol(f->deg, f->raf, ipgL, xref, &wpg3, NULL);
+		    // mapping from the ref glop to the physical glop
+		    real dtau[3][3], codtau[3][3];
+		    Ref2Phy(f->physnode,
+			    xref,
+			  NULL, // dphiref
+			    -1,  // ifa
+			    NULL, // xphy
+			    dtau,
+			    codtau,
+			    NULL, // dphi
+			    NULL);  // vnds
+		    // we compute ourself the normal vector because we
+		    // have to take into account the subcell surface
+		    
+		    real h1h2 = 1. / nraf[dim1] / nraf[dim2];
+		    vnds[0] = codtau[0][dim0] * h1h2;
+		    vnds[1] = codtau[1][dim0] * h1h2;
+		    vnds[2] = codtau[2][dim0] * h1h2;
+		  }
+
+		  // numerical flux from the left and right state and
+		  // normal vector
+		  real wL[m], wR[m], flux[m];
+		  for(int iv = 0; iv < m; iv++) {
+		    // TO DO change the varindex signature
+		    int imemL = f->varindex(f->deg, f->raf, f->model.m, ipgL, iv); 
+		    int imemR = f->varindex(f->deg, f->raf, f->model.m, ipgR, iv);
+		    // end TO DO
+		    wL[iv] = w[imemL];
+		    wR[iv] = w[imemR];
+		  }
+		  f->model.NumFlux(wL, wR, vnds, flux);
+		  
+		  // subcell ref surface glop weight
+		  real wpg
+		  = wglop(deg[dim1], iL[dim1])
+		    * wglop(deg[dim2], iL[dim2]);
+		  
+		  /* printf("vnds %f %f %f flux %f wpg %f\n", */
+		/* 	 vnds[0], vnds[1], vnds[2], */
+		/* 	 flux[0], wpg); */
+		  
+		// finally distribute the flux on the two sides
+		  for(int iv = 0; iv < m; iv++) {
+		    // TO DO change the varindex signature
+		    int imemL = f->varindex(f->deg, f->raf, f->model.m, ipgL, iv);
+		    int imemR = f->varindex(f->deg, f->raf, f->model.m, ipgR, iv);
+		    // end TO DO
+		    nlv[imemL] -= flux[iv] * wpg;
+		    nlv[imemR] += flux[iv] * wpg;
+		  }
+		  
+		}  // face yhat loop
+	      } // face xhat loop
+	    } // endif internal face
+	  } // dim loop
+
+	///////////// Volume part //////////////////////
+	real *xref0 = malloc(sc_npg * sizeof(real));
+	real *xref1 = malloc(sc_npg * sizeof(real));
+	real *xref2 = malloc(sc_npg * sizeof(real));
+	real *omega = malloc(sc_npg * sizeof(real));
+	int *imems = malloc(m * sc_npg * sizeof(int));
+	int pos = 0;
+	for(unsigned int p = 0; p < sc_npg; ++p) {
+	  real xref[3];
+	  real tomega;
+
+	  ref_pg_vol(f->deg, f->raf, offsetL + p, xref, &tomega, NULL);
+	  xref0[p] = xref[0];
+	  xref1[p] = xref[1];
+	  xref2[p] = xref[2];
+	  omega[p] = tomega;
+
+	  for(int im = 0; im < m; ++im) {
+	    imems[pos++] = f->varindex(f->deg,f->raf,f->model.m, offsetL + p, im);
+	  }
+	}
+
+	// loop in the "cross" in the three directions
+	for(int dim0 = 0; dim0 < 3; dim0++) {
+	  //for(int dim0 = 0; dim0 < 2; dim0++) {  // TODO : return to 3d !
+	  // point p at which we compute the flux
+
+	  for(int p0 = 0; p0 < npg[0]; p0++) {
+	    for(int p1 = 0; p1 < npg[1]; p1++) {
+	      for(int p2 = 0; p2 < npg[2]; p2++) {
+		real wL[m], flux[m];
+		int p[3] = {p0, p1, p2};
+		int ipgL = offsetL + p[0] + npg[0] * (p[1] + npg[1] * p[2]);
+		for(int iv = 0; iv < m; iv++) {
+		  ///int imemL = f->varindex(f_interp_param, ie, ipgL, iv);
+		  wL[iv] = w[imems[m * (ipgL - offsetL) + iv]];
+		}
+		int q[3] = {p[0], p[1], p[2]};
+		// loop on the direction dim0 on the "cross"
+		for(int iq = 0; iq < npg[dim0]; iq++) {
+		  q[dim0] = (p[dim0] + iq) % npg[dim0];
+		  real dphiref[3] = {0, 0, 0};
+		  // compute grad phi_q at glop p
+		  dphiref[dim0] = dlag(deg[dim0], q[dim0], p[dim0]) 
+		    * nraf[dim0];
+
+		  real xrefL[3] = {xref0[ipgL - offsetL],
+				   xref1[ipgL - offsetL],
+				   xref2[ipgL - offsetL]};
+		  real wpgL = omega[ipgL - offsetL];
+		  /* real xrefL[3], wpgL; */
+		  /* ref_pg_vol(f->interp_param+1,ipgL,xrefL, &wpgL, NULL); */
+
+		  // mapping from the ref glop to the physical glop
+		  real dtau[3][3], codtau[3][3], dphiL[3];
+		  Ref2Phy(f->physnode,
+			  xrefL,
+			  dphiref, // dphiref
+			  -1,  // ifa
+			  NULL, // xphy
+			  dtau,
+			  codtau,
+			  dphiL, // dphi
+			  NULL);  // vnds
+
+		  f->model.NumFlux(wL, wL, dphiL, flux);
+
+		  int ipgR = offsetL+q[0]+npg[0]*(q[1]+npg[1]*q[2]);
+		  for(int iv = 0; iv < m; iv++) {
+		    int imemR = f->varindex(f->deg,f->raf,f->model.m, ipgR, iv);
+		    int temp = m * (ipgR - offsetL) + iv;  
+		    assert(imemR == imems[temp]);
+		    nlv[imems[temp]] += flux[iv] * wpgL;
+		  }
+		} // iq
+	      } // p2
+	    } // p1
+	  } // p0
+
+	} // dim loop
+
+	free(omega);
+	free(xref0);
+	free(xref1);
+	free(xref2);
+	free(imems);
+	  
+	} // subcell icl2 loop
+      } // subcell icl1 loop
+    } // subcell icl0 loop
+
+    //////////////////// source and mass
+    for(int ipg = 0; ipg < NPG(f->deg, f->raf); ipg++) {
+      real dtau[3][3], codtau[3][3], xpgref[3], xphy[3], wpg;
+      ref_pg_vol(f->deg, f->raf, ipg, xpgref, &wpg, NULL);
+      Ref2Phy(f->physnode, // phys. nodes
+	      xpgref, // xref
+	      NULL, -1, // dpsiref, ifa
+	      xphy, dtau, // xphy, dtau
+	      codtau, NULL, NULL); // codtau, dpsi, vnds
+      real det = dot_product(dtau[0], codtau[0]);  //// temp !!!!!!!!!!!!!!!
+      real wL[m], source[m];
+      for(int iv = 0; iv < m; ++iv){
+	int imem = f->varindex(f->deg, f->raf, f->model.m, ipg, iv);
+	wL[iv] = w[imem];
+      }
+      
+      f->model.Source(xphy, f->tnow, wL, source);
+      // printf("tnow=%f\n",f->tnow);
+
+      for(int iv = 0; iv < m; ++iv) {
+	int imem = f->varindex(f->deg, f->raf, f->model.m, ipg, iv);
+	nlv[imem] += source[iv] * det * wpg;
+	nlv[imem] /=(wpg * det); //+=wL[iv] * (wpg * det);	
+      }
+    }
+    
+
+  }
+  
 
 }
