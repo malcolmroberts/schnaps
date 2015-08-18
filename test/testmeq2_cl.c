de919f9d9aaac100fc76a79eaeda7f2497ed2cc0
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2015-08-18 12:50:55 -0600

    Fix testmeq2_cl bug where boundary terms were not computed in OpenCL version

--- test/testmeq2_cl.c
+++ test/testmeq2_cl.c
@@ -52,19 +52,19 @@ int TestmEq2(void) {
   strcat(cl_buildoptions, buf);
 
   // Read the gmsh file
-  ReadMacroMesh(&(f.macromesh), "test/testcube.msh");
+  ReadMacroMesh(&f.macromesh, "test/testcube.msh");
 
   // Try to detect a 2d mesh
-  Detect2DMacroMesh(&(f.macromesh));
+  Detect2DMacroMesh(&f.macromesh);
   assert(f.macromesh.is2d);
 
-  BuildConnectivity(&(f.macromesh));
+  BuildConnectivity(&f.macromesh);
 
-  //AffineMapMacroMesh(&(f.macromesh));
+  //AffineMapMacroMesh(&f.macromesh);
  
   Initfield(&f);
   
-  CheckMacroMesh(&(f.macromesh), f.interp.interp_param + 1);
+  CheckMacroMesh(&f.macromesh, f.interp.interp_param + 1);
 
   real *dtwn_cl = f.dtwn;
   real *dtwn = calloc(f.wsize, sizeof(real));
@@ -81,15 +81,11 @@ int TestmEq2(void) {
   
   f.dtwn = dtwn_cl;
 
-  set_buf_to_zero_cl(&(f.dtwn_cl), f.wsize, &f, 0, NULL, NULL);
+  set_buf_to_zero_cl(&f.dtwn_cl, f.wsize, &f, 0, NULL, NULL);
   clFinish(f.cli.commandqueue);
 
   for(int ie = 0; ie < f.macromesh.nbelems; ++ie) {
-    /* update_physnode_cl(&f, ie, f.physnode_cl, f.physnode, NULL, */
-    /* 		       0, NULL, NULL); */
-    /* clFinish(f.cli.commandqueue); */
-
-    DGVolume_CL(ie, &f, &(f.wn_cl), 0, NULL, NULL);
+    DGVolume_CL(ie, &f, &f.wn_cl, 0, NULL, NULL);
     clFinish(f.cli.commandqueue);
   }
 
@@ -100,7 +96,6 @@ int TestmEq2(void) {
   for(int ie = 0; ie < f.macromesh.nbelems; ++ie) {
     DGVolume(ie, &f, f.wn, f.dtwn);
   }
-
 
   const int nraf[3] = {f.interp_param[4],
 		       f.interp_param[5],
@@ -113,19 +108,20 @@ int TestmEq2(void) {
 		      deg[1] + 1,
 		      deg[2] + 1};
   
-  for(int ie = 0; ie < f.macromesh.nbelems; ++ie){
-    for(int ic=0;ic<nraf[0]*nraf[1]*nraf[2];++ic){
-      for(int ipg=0;ipg<npg[0]*npg[1]*npg[2];++ipg){
-	printf("ie=%d ic=%d ipg=%d ",ie,ic,ipg);
-	for(int iv=0;iv<2;++iv){
-	  int imem=f.varindex(f.interp_param,ie,ipg+ic*NPG(f.interp_param+1), iv); 
+  for(int ie = 0; ie < f.macromesh.nbelems; ++ie) {
+    for(int ic = 0; ic < nraf[0] * nraf[1] * nraf[2];++ic) {
+      for(int ipg = 0; ipg < npg[0] * npg[1] * npg[2]; ++ipg) {
+	printf("ie=%d ic=%d ipg=%d ", ie, ic, ipg);
+	for(int iv = 0; iv < 2; ++iv) {
+	  int imem = f.varindex(f.interp_param,ie,ipg+ic*NPG(f.interp_param+1), 
+				iv); 
 	  //printf("NPG=%d\n",NPG(f.interp_param+1));
-	  printf("iv=%d \ndtwcl=%f dtwcpu=%f\n\n",iv,dtwn_cl[imem],dtwn[imem]);
+	  printf("iv=%d \ndtwcl=%f dtwcpu=%f\n\n", 
+		 iv, dtwn_cl[imem], dtwn[imem]);
 	}
       }
     }
   }
-
 
   err = maxerr(dtwn, dtwn_cl, f.wsize);
   printf("\tmax error: %f\n", err);
@@ -139,22 +135,18 @@ int TestmEq2(void) {
   }
   f.dtwn = dtwn_cl;
 
-  set_buf_to_zero_cl(&(f.dtwn_cl), f.wsize, &f, 0, NULL, NULL);
+  set_buf_to_zero_cl(&f.dtwn_cl, f.wsize, &f, 0, NULL, NULL);
   clFinish(f.cli.commandqueue);
 
   for(int ie = 0; ie < f.macromesh.nbelems; ++ie) {
-    /* update_physnode_cl(&f, ie, f.physnode_cl, f.physnode, NULL, */
-    /* 		       0, NULL, NULL); */
-    /* clFinish(f.cli.commandqueue); */
-    
-    DGFlux_CL(&f, 0, ie, &(f.wn_cl), 0, NULL, NULL);
+    DGFlux_CL(&f, 0, ie, &f.wn_cl, 0, NULL, NULL);
     clFinish(f.cli.commandqueue);
 
-    DGFlux_CL(&f, 1, ie, &(f.wn_cl), 0, NULL, NULL);
+    DGFlux_CL(&f, 1, ie, &f.wn_cl, 0, NULL, NULL);
     clFinish(f.cli.commandqueue);
 
     if(!f.macromesh.is2d) {
-      DGFlux_CL(&f, 2, ie, &(f.wn_cl), 0, NULL, NULL);
+      DGFlux_CL(&f, 2, ie, &f.wn_cl, 0, NULL, NULL);
       clFinish(f.cli.commandqueue);
     }
   }
@@ -178,13 +170,26 @@ int TestmEq2(void) {
   }
 
   f.dtwn = dtwn_cl;
-  set_buf_to_zero_cl(&(f.dtwn_cl), f.wsize, &f, 0, NULL, NULL);
+
+  set_buf_to_zero_cl(&f.dtwn_cl, f.wsize, &f, 0, NULL, NULL);
   clFinish(f.cli.commandqueue);
-  for(int ifa = 0; ifa < f.macromesh.nbfaces; ++ifa) {
-    DGMacroCellInterface_CL(ifa, &f, &f.wn_cl,
-    			    0, NULL, NULL);
+
+  const int ninterfaces = f.macromesh.nmacrointerfaces;
+  for(int i = 0; i < ninterfaces; ++i) {
+    int ifa = f.macromesh.macrointerface[i];
+    DGMacroCellInterface_CL(ifa, &f, &(f.wn_cl),
+			    0, NULL, NULL);
     clFinish(f.cli.commandqueue);
   }
+  
+  const int nboundaryfaces = f.macromesh.nboundaryfaces;
+  for(int i = 0; i < nboundaryfaces; ++i) {
+    int ifa = f.macromesh.boundaryface[i];
+    DGBoundary_CL(ifa, &f, &(f.wn_cl),
+			    0, NULL, NULL);
+    clFinish(f.cli.commandqueue);
+  }
+
   CopyfieldtoCPU(&f);
   clFinish(f.cli.commandqueue);
   
@@ -203,7 +208,7 @@ int TestmEq2(void) {
   }
   
   f.dtwn = dtwn_cl;
-  dtfield_CL(&f, &(f.wn_cl), 0, NULL, NULL);
+  dtfield_CL(&f, &f.wn_cl, 0, NULL, NULL);
   clFinish(f.cli.commandqueue);
   
   CopyfieldtoCPU(&f);
