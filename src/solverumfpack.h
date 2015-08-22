e382d97b413c4c0d3ce17eae297a6c32ca605b3f
Philippe Helluy <helluy@math.unistra.fr>
2015-08-22 23:16:52 +0200

    small modifs for mac

--- src/solverumfpack.h
+++ src/solverumfpack.h
@@ -1,7 +1,11 @@
 #ifndef _SOLVERUMFPACK_H
 #define _SOLVERUMFPACK_H
 
+#ifdef __APPLE__
+#include "umfpack.h"
+#else
 #include "suitesparse/umfpack.h"
+#endif
 #include "linear_solver.h"
 
 int smalltestumfpack(void);
