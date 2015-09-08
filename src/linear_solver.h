178e7cda23d84758a025e34870c93399bfa9ef06
efranck21 <franck@m-franck.u-strasbg.fr>
2015-09-08 09:31:49 +0200

    change linear solver

--- src/linear_solver.h
+++ src/linear_solver.h
@@ -3,12 +3,11 @@
 
 #include "global.h"
 #include <stdbool.h>
-#include "field.h"
-
+#include "paralution_c.h"
 
 typedef enum MatrixStorage{SKYLINE,CSR} MatrixStorage;
 typedef enum Solver{LU,GMRES,PAR_GMRES,PAR_FGMRES,PAR_CG,PAR_BICGSTAB,PAR_AMG,PAR_LU,PAR_QR} Solver;
-typedef enum PC{NONE,PAR_JACOBI,PAR_ILU,PAR_MULTICOLOREDSGS,PAR_MULTICOLOREDGS,PAR_MULTICOLOREDILU,PAR_AMG_PC,PAR_ELIMI} PC;
+typedef enum PC{NONE,JACOBI,PAR_JACOBI,PAR_ILU,PAR_MULTICOLOREDSGS,PAR_MULTICOLOREDGS,PAR_MULTICOLOREDILU,PAR_AMG_PC,PAR_ELIMI,PHY_BASED,PHY_BASED_EXACT,EXACT,PHDF} PC;
 
 //! class for managing linear solvers
 typedef struct LinearSolver{
@@ -41,6 +40,9 @@ typedef struct LinearSolver{
   //! solver type;
   Solver solver_type;
 
+  //! solver type;
+  bool is_CG;
+
   //! name of the storage method;
   PC pc_type; 
 
@@ -66,53 +68,6 @@ typedef struct LinearSolver{
 
 } LinearSolver;
 
-
-typedef struct JFLinearSolver{
-
-  //! \brief number of equations
-  int neq;
-
-  //! brief eps for free Jacobian matrix
-  real eps;
-
-  //! solver type;
-  Solver solver_type;
-
-  //! name of the storage method;
-  PC pc_type; 
-
-  //! \brief solution of the linear system
-  real* sol;
-  //! \brief rhs of the linear system
-  real* rhs;
-  //! \brief sol at the time n
-  real* soln;
-
-    //! tolerance iterative solver
-  real tol;
-
-  //! restart for gmres
-  int restart_gmres;
-  //! number max of iteration
-  int iter_max;
-
-  //! \brief compute a matrix vector product
-  //! \param[in] lsol the LinearSolver object containing matrix A
-  //! \param[in] f the field
-  //! \param[in] x a vector
-  //! \param[out] prod Ax
-  void (*MatVecProduct)(void* lsol,field * f,real x[],real prod[]);
-
-  //! \brief compute the
-  //! \param[in] lsol the LinearSolver object containing matrix A
-  //! \param[in] f the field
-  //! \param[in] solvector the solution at the time n
-  //! \param[out] given the nonlinear vector for the free jacobian
-  void (*NonlinearVector_computation)(void* lsol,field * f,real * solvector,real *nlvector);
-
-} JFLinearSolver;
-
-
 //! \brief init the LinearSolver structure with an empty matrix
 //! \param[inout] lsol the LinearSolver object
 //! \param[in] n number of equations
@@ -124,6 +79,7 @@ void InitLinearSolver(LinearSolver* lsol,int n,
 
 //! \brief free the allocated arrays
 //! \param[inout] lsol the LinearSolver object
+//! \param[in] freeAll: an integer whose purpose is to choose whether we free everything. (testing purposes)
 void FreeLinearSolver(LinearSolver* lsol);
 
 
@@ -170,43 +126,20 @@ void DisplayLinearSolver(LinearSolver* lsol);
 //! \param[out] prod Ax
 void MatVect(void * system,real x[],real prod[]);
 
+//! \brief compute a matrix vector product
+//! \param[in] system the LinearSolver object containing matrix A
+//! \param[in] x a vector
+//! \param[out] prod Ax
+void MatVect_slow(void * system,real x[],real prod[]);
+
 //! \brief compute the inplace LU decomposition
 //! \param[inout] lsol the LinearSolver object
 void LUDecompLinearSolver(LinearSolver* lsol);
 
 //! \brief solve the linear system
 //! \param[in] lsol the LinearSolver object
+//! \param[in] simu the Simulation object
 void SolveLinearSolver(LinearSolver* lsol);
-
-
-
-
-//! \brief init the LinearSolver structure with an empty matrix
-//! \param[inout] lsol the LinearSolver object
-//! \param[in] n number of equations
-//! \param[in] solvtyp solver type (optional)
-void InitJFLinearSolver(JFLinearSolver* lsol,int n,
-		      Solver* solvtyp);
-
-//! \brief free the allocated arrays
-//! \param[inout] lsol the LinearSolver object
-void FreeJFLinearSolver(JFLinearSolver* lsol);
-
-
-
-//! \brief compute a matrix vector product
-//! \param[in] system the LinearSolver object containing matrix A
-//! \param[in] f a field
-//! \param[in] x a vector
-//! \param[out] prod Ax
-void MatVecJacobianFree(void * system,field * f,real x[],real prod[]);
-
-//! \brief solve the linear system
-//! \param[inout] lsol the JFLinearSolver object
-//! \param[in] f field asscoiated
-void SolveJFLinearSolver(JFLinearSolver* lsol,field *f);
-
-
 
 
 
@@ -227,13 +160,17 @@ real Vector_prodot(real x[],real y[],int N);
 //! \param[in] N size
 real Vector_norm2(real x[],int  N);
 
-//! \brief solve the linear system with paralution
-//! \param[in] lsol contains the matrices rhs and sol
-void Solver_Paralution(LinearSolver* lsol);
-
 //! \brief solve the linear system with the GMREs of the cerfacs
 //! \param[in] lsol contains the matrices rhs and sol
 void GMRESSolver(LinearSolver* lsol);
 
+
+//! \brief solve the linear system with paralution
+//! \param[in] lsol contains the matrices rhs and sol
+void Solver_Paralution(LinearSolver* lsol);
+
+//! \brief Jacobi preconditioner
+//! \param[in] lsol contains the matrices rhs and sol
+void Jacobi_PC(LinearSolver* lsol, real* sol, real* rhs);
 
 #endif
