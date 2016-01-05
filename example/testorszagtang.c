801fd53e41466561fd9195b7e755a6e0cfa1226a
Michel Massaro <massaro@irma-atlas.u-strasbg.fr>
2016-01-05 16:31:50 +0100

    OrszagTang in progress

--- example/testorszagtang.c
+++ example/testorszagtang.c
@@ -27,7 +27,85 @@ int main(int argc, char *argv[]) {
   return !resu;
 }
 
-int TestOrszagTang(int argc, char *argv[]) { /*
+int TestOrszagTang(int argc, char *argv[]) {
+
+
+  int test = true;
+
+  if(!cldevice_is_acceptable(nplatform_cl, ndevice_cl)) {
+    printf("OpenCL device not acceptable.\n");
+    return true;
+  }
+
+  Simulation simu;
+  EmptySimulation(&simu);
+
+
+  MacroMesh mesh;
+  char *mshname =  "../test/disque2d.msh";
+  
+  ReadMacroMesh(&mesh, mshname);
+  Detect2DMacroMesh(&mesh);
+  bool is2d=mesh.is2d; 
+  assert(is2d);  
+
+  real periodsize = 6.2831853;
+  //mesh.period[0]=periodsize;
+  //mesh.period[1]=periodsize;
+
+  BuildConnectivity(&mesh);
+  int deg[]={1, 1, 0};
+  int raf[]={10, 10, 1};
+  CheckMacroMesh(&mesh, deg, raf);
+
+  Model model;
+
+  real cfl = 0.2;
+
+  simu.cfl = cfl;
+  model.m = 9;
+
+  strcpy(model.name,"MHD");
+
+  model.NumFlux=MHDNumFluxRusanov;
+  model.BoundaryFlux=MHDBoundaryFluxOrszagTang;
+  model.InitData=MHDInitDataOrszagTang;
+  model.ImposedData=MHDImposedDataOrszagTang;
+  model.Source = NULL;
+  
+  char buf[1000];
+  sprintf(buf, "-D _M=%d -D _PERIODX=%f -D _PERIODY=%f",
+          model.m,
+          periodsize,
+          periodsize);
+  strcat(cl_buildoptions, buf);
+
+  sprintf(numflux_cl_name, "%s", "MHDNumFluxRusanov");
+  sprintf(buf," -D NUMFLUX=");
+  strcat(buf, numflux_cl_name);
+  strcat(cl_buildoptions, buf);
+
+  sprintf(buf, " -D BOUNDARYFLUX=%s", "MHDBoundaryFluxOrszagTang");
+  strcat(cl_buildoptions, buf);
+
+
+
+  InitSimulation(&simu, &mesh, deg, raf, &model);
+ 
+  real tmax = 0.1;
+  simu.vmax = 6.0;
+  real dt = 0;
+  RK2_CL(&simu, tmax, dt,  0, NULL, NULL);
+  
+  CopyfieldtoCPU(&simu);
+ 
+  PlotFields(0, false, &simu, NULL, "dgvisu.msh");
+  show_cl_timing(&simu);
+
+  return test;
+}
+
+  /*
   real cfl = 0.2;
   real tmax = 0.1;
   bool writemsh = false;
@@ -152,5 +230,6 @@ int TestOrszagTang(int argc, char *argv[]) { /*
   //Gnuplot(&f,0,0.0,"data1D.dat");
 
 
-  return test; */
-}
+  return test;
+
+  }*/
