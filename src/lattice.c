bb5b932709b83bfef1b2cbdca72b7a9dcf751cfd
efranck21 <franck@m-franck.u-strasbg.fr>
2016-03-21 11:21:25 +0100

    add flux lattice

---
+++ src/lattice.c
@@ -0,0 +1,38 @@
+#include "lattice.h"
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+#include "interpolation.h"
+#include "geometry.h"
+#include "skyline.h"
+
+
+
+void Lattice_NumFlux(schnaps_real wL[],schnaps_real wR[],schnaps_real* vnorm,schnaps_real* flux){
+ LatticeData * ld=&schnaps_lattice_data;
+ 
+  for(int i = 0;i < ld->index_max_q+1;i++){
+    schnaps_real vn=0;
+    for(int dim = 0;dim < ld->d; dim++){
+      vn + = ld->q_tab[i][dim]*vnorm[dim];
+    }
+    
+    schnaps_real vnp = vn>0 ? vn : 0;
+    schnaps_real vnm = vn-vnp;
+
+    flux[i] = vnp * wL[i] + vnm * wR[i];
+  }
+
+  flux[kd->index_rho]=0; 
+  flux[kd->index_ux]=0; 
+  flux[kd->index_uy]=0;
+  flux[kd->index_uz]=0;
+  flux[kd->index_temp]=0; 
+  flux[kd->index_p]=0; 
+
+};
+
+
+
+
+
